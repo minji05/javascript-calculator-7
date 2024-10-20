408a6cda88c7cf54e7f495eb36525f876b93b94c
minji05 <smj95133@likelion.org>
2024-10-21 01:59:06 +0900

    refactor: 입력 처리 함수를 하나의 run 메서드로 통합

--- src/App.js
+++ src/App.js
@@ -2,18 +2,9 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const input = await this.getUserInput();
-    this.printResult(input);
-  }
-
-  // 사용자에게 입력을 받는 메서드
-  static async getUserInput() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
+    Console.print(`입력된 문자열: ${input}`);
     return input;
-  }
-
-  static printResult(input) {
-    Console.print(`입력한 문자열: ${input}`);
   }
 }
 
