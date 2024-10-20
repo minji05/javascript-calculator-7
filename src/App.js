f96f5bcb687a58d091cd524f7428512fbaa6a594
minji05 <smj95133@likelion.org>
2024-10-21 02:29:36 +0900

    refactor: 입력된 값이 쉼표 또는 콜론을 구분자로 가지는 문자열인지 확인

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ class App {
   async run() {
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.');
     Console.print(`입력된 문자열: ${input}`);
+    this.processDelimiters(input);
     return input;
+  }
+
+  processDelimiters(input) {
+    if (input.includes(',') || input.includes(':')) {
+      Console.print(`쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열`);
+    }
   }
 }
 
